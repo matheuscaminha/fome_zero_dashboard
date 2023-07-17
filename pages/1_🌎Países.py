9c32f668d2a07bbf55fdd1a9f0911c2d132e2330
matheuscaminha <134650122+matheuscaminha@users.noreply.github.com>
2023-07-16 22:31:59 -0300

    Update 1_🌎Países.py

--- pages/1_🌎Países.py
+++ pages/1_🌎Países.py
@@ -6,7 +6,7 @@ import inflection
 
 st.set_page_config(page_title='Países', page_icon='🌎', layout='wide')
 
-df = pd.read_csv('\dataset\zomato.csv')
+df = pd.read_csv('dataset\zomato.csv')
 
 # ---------------------------------------------- #
 #### LIMPEZA DE DADOS ####
